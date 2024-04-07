e6e13a77962d1473c34e70c777256e0003aaba15
Shikhar Baniya <47479686+shikhar-baniya@users.noreply.github.com>
2024-04-07 07:48:00 +0000

    added template

--- perspective-simulator/src/App.vue
+++ perspective-simulator/src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>CSS3 Perspective Playground</h2>
+    <main>
+      <section class="settings">
+        <div class="settings-container">
+          <label>perspective: 0px;</label>
+          <input type="range" min="0" max="999" />
+
+          <label>rotateX: 0deg; </label>
+          <input type="range" min="-180" max="180" />
+
+          <label>rotateY: 0deg; </label>
+          <input type="range" min="-180" max="180" />
+
+          <label>rotateZ: 0deg; </label>
+          <input type="range" min="-180" max="180" />
+
+          <button type="button">Reset</button>
+          <button type="button">Copy</button>
+        </div>
+      </section>
+      <section class="output">
+        <div class="box-container">
+          <div class="box"></div>
+        </div>
+      </section>
+    </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
