3fb20dfa043758f51d6a5ca2daec3b0d6b3e8ecf
SBA49 <shikhar.baniya@3ds.com>
2024-04-07 18:05:23 +0530

    Completed the Perspective Simulator Project

--- perspective-simulator/src/App.vue
+++ perspective-simulator/src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <h2>CSS3 Perspective Playground</h2>
+  <h2>CSS3 Perspective Simulator</h2>
     <main>
       <section class="settings">
         <div class="settings-container">
-          <label>perspective: 0px;</label>
-          <input type="range" min="0" max="999" />
+          <label>perspective: {{perspective}}px;</label>
+          <input v-model="perspective" type="range" min="0" max="999" />
 
-          <label>rotateX: 0deg; </label>
-          <input type="range" min="-180" max="180" />
+          <label>rotateX: {{rotateX}}deg; </label>
+          <input v-model="rotateX" type="range" min="-180" max="180" />
 
-          <label>rotateY: 0deg; </label>
-          <input type="range" min="-180" max="180" />
+          <label>rotateY: {{rotateY}}deg; </label>
+          <input v-model="rotateY" type="range" min="-180" max="180" />
 
-          <label>rotateZ: 0deg; </label>
-          <input type="range" min="-180" max="180" />
+          <label>rotateZ: {{rotateZ}}deg; </label>
+          <input v-model="rotateZ" type="range" min="-180" max="180" />
 
-          <button type="button">Reset</button>
-          <button type="button">Copy</button>
+          <div class="showStyle" :class="isCopy ? 'copied' : 'notCopied'">{{ box }}</div>
+
+          <div class="actionBtn">
+            <button class="reset" type="button" @click="reset">Reset</button>
+            <button class="copy" type="button" @click="copyStyles">Copy</button>
+          </div>
         </div>
       </section>
       <section class="output">
         <div class="box-container">
-          <div class="box"></div>
+          <div class="box" :style="box"></div>
         </div>
       </section>
     </main>
@@ -31,10 +35,42 @@
 
 export default {
   name: 'App',
-  
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+      isCopy: false
+    }
+  },
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+    copyStyles() {
+      this.isCopy = true;
+      const style = `transform: ${this.box.transform};`;
+      navigator.clipboard.writeText(style).then(() => {
+        setTimeout(() => { 
+          this.isCopy = false;
+      }, 3000)
+      }).catch((error) => {
+        console.error('Failed to copy styles: ', error);
+      });
+    }
+  },
+  computed: {
+    box() {
+      return {
+        transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style src="./assets/style.css" />
